fe47e8dcf49f0423b99b73e7893f5c5fd0e27836
saintwolf <judiconis@gmail.com>
2015-08-04 04:15:14 +0100

    Updated web server package list
    Added php5-intl to package list to allow installation of Symfony2

--- setup_server.sh
+++ setup_server.sh
@@ -79,7 +79,7 @@ function setup_security_apps
 function setup_lamp_server
 {
 	# Sets up Apache2, MySQL and PHP
-	apt-get install -y apache2 php5-mysql mysql-server php5 libapache2-mod-php5
+	apt-get install -y apache2 php5-mysql mysql-server php5 libapache2-mod-php5 php5-intl
 
 	# Do Apache stuff
 	mkdir /var/virtualsites

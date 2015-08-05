fde5276ac9e2c2b7fa4c1a3e2559082783846f03
John Good <judiconis@gmail.com>
2015-08-05 01:48:56 +0000

    Modified script to wget latest versions of fail2ban jail scripts without full repo downloaded

--- setup_server.sh
+++ setup_server.sh
@@ -56,9 +56,10 @@ function setup_security_apps
 	sed -i '/\[apache-noscript\]/,/enabled =.*/{s/enabled.*/enabled = true/g}' /etc/fail2ban/jail.local
 	sed -i '/\[apache-overflows\]/,/enabled =.*/{s/enabled.*/enabled = true/g}' /etc/fail2ban/jail.local
 
+	wget -P /etc/fail2ban/filter.d https://raw.githubusercontent.com/saintwolf/ServerScripts/master/conf/fail2ban/filter.d/apache-phpmyadmin.conf
+	wget -P /etc/fail2ban/filter.d https://raw.githubusercontent.com/saintwolf/ServerScripts/master/conf/fail2ban/filter.d/apache-postflood.conf
 	echo -e "\n\n\n[apache-phpmyadmin]\nenabled  = true\nport     = http,https\nfilter   = apache-phpmyadmin\nlogpath  = /var/log/apache*/*error.log\nmaxretry = 3" >> /etc/fail2ban/jail.local
 	echo -e "\n\n\n[apache-postflood]\n\nenabled = true\nfilter = apache-postflood\nlogpath = /var/log/httpd/access_log\nfindtime = 10\nmaxretry = 10" >> /etc/fail2ban/jail.local
-	cp conf/fail2ban/filter.d/* /etc/fail2ban/filter.d/
 	
 	# Install and set up RKHunter
 	apt-get install -y rkhunter

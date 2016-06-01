cff9c49a212bff6edeecd5f552f74f45c424c211
John Good <judiconis@gmail.com>
2016-06-01 14:25:49 +0100

    Updated LAMP setup for PHP7

--- setup_server.sh
+++ setup_server.sh
@@ -84,7 +84,7 @@ function setup_security_apps
 function setup_lamp_server
 {
 	# Sets up Apache2, MySQL and PHP
-	apt-get install -y apache2 php5-mysql mysql-server php5 libapache2-mod-php5 php5-intl php5-curl npm
+	apt-get install -y apache2 php-mysql mysql-server php libapache2-mod-php php-intl php-curl npm
 
 	# Do Apache stuff
 	mkdir /var/virtualsites
